60016de4f4f9b0ffb2716485bd12d8bfb00f32e6
small-dogg <sy920112@naver.com>
2022-04-24 16:43:01 +0900

    feat: 상품 도메인 개발   - 상품 엔티티 개발(비즈니스 로직 추가)   - 상품 리포지토리 개발   - 상품 서비스 개발

--- src/main/java/com/smalldogg/jpashop/domain/item/Item.java
+++ src/main/java/com/smalldogg/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.smalldogg.jpashop.domain.item;
 
 import com.smalldogg.jpashop.domain.Category;
+import com.smalldogg.jpashop.exception.NotEnoughStockException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -33,4 +34,25 @@ public abstract class Item {
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
 
+  //==비즈니스 로직==//
+
+  /**
+   * stock 증가
+   * @param quantity
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   * @param quantity
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if(restStock <0){
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
---
+++ src/main/java/com/smalldogg/jpashop/service/ItemService.java
@@ -0,0 +1,28 @@
+package com.smalldogg.jpashop.service;
+
+import com.smalldogg.jpashop.domain.item.Item;
+import com.smalldogg.jpashop.repository.ItemRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+  private final ItemRepository itemRepository;
+
+  @Transactional
+  public void saveItem(Item item) {
+    itemRepository.save(item);
+  }
+
+  public List<Item> findItems() {
+    return itemRepository.findAll();
+  }
+
+  public Item findOne(Long itemId) {
+    return itemRepository.findOne(itemId);
+  }
+}
