65b442c2fd93f3f3733ac7d63e1e24ca8010fda1
small-dogg <sy920112@naver.com>
2022-04-28 10:13:16 +0900

    feat: API 개발 고급 - 지연 로딩과 조회 성능 최적화  - 간단한 주문 조회 V1: 엔티티를 직접 노출  - 간단한 주문 조회 V2: 엔티티를 DTO로 변환  - 간단한 주문 조회 V3: 페치 조인 최적화

--- src/main/java/com/smalldogg/jpashop/repository/OrderRepository.java
+++ src/main/java/com/smalldogg/jpashop/repository/OrderRepository.java
@@ -1,93 +1,106 @@
 package com.smalldogg.jpashop.repository;
 
 import com.smalldogg.jpashop.domain.Order;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
 
-  private final EntityManager em;
+    private final EntityManager em;
 
-  public void save(Order order) {
-    em.persist(order);
-  }
-
-  public Order findOne(Long id) {
-    return em.find(Order.class, id);
-  }
-
-  /**
-   *String으로 조건에 대한 동적 쿼리(권장x)
-   */
-
-  public List<Order> findAllByString(OrderSearch orderSearch) {
-    //동적 쿼리 처리 방법 1
-
-    String jpql = "select o from Order o join o.member m";
-    boolean isFirstCondition = true;
-
-    if (orderSearch.getOrderStatus() != null) {
-      if (isFirstCondition) {
-        jpql += " where";
-        isFirstCondition = false;
-      } else {
-        jpql += " and";
-      }
-      jpql += " o.status = :status";
+    public void save(Order order) {
+        em.persist(order);
     }
 
-    if (StringUtils.hasText(orderSearch.getMemberName())) {
-      if (isFirstCondition) {
-        jpql += " where";
-        isFirstCondition = false;
-      } else {
-        jpql += " and";
-      }
-      jpql += " m.name like :name";
-    }
-    TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-        .setMaxResults(1000);
-
-    if (orderSearch.getOrderStatus() != null) {
-      query = query.setParameter("status", orderSearch.getOrderStatus());
-    }
-    if (orderSearch.getMemberName() != null) {
-      query = query.setParameter("name", orderSearch.getMemberName());
-    }
-    return query.getResultList();
-  }
-
-  /**
-   * JPA Criteria를 사용한 동적 쿼리(권장x)
-   */
-  public List<Order> findAllByCriteria(OrderSearch orderSearch){
-    CriteriaBuilder cb = em.getCriteriaBuilder();
-    CriteriaQuery<Order> cq = cb.createQuery(Order.class);
-    Root<Order> o = cq.from(Order.class);
-    Join<Object, Object> m = o.join("member", JoinType.INNER);
-
-    List<Predicate> criteria = new ArrayList<>();
-
-    if (orderSearch.getOrderStatus() != null) {
-      Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
-      criteria.add(status);
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
     }
 
-    cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
-    TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
-    return query.getResultList();
-  }
+    /**
+     * String으로 조건에 대한 동적 쿼리(권장x)
+     */
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        //동적 쿼리 처리 방법 1
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (orderSearch.getMemberName() != null) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
+    /**
+     * JPA Criteria를 사용한 동적 쿼리(권장x)
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery(
+                "select new com.smalldogg.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
 }
---
+++ src/main/java/com/smalldogg/jpashop/repository/OrderSimpleQueryDto.java
@@ -0,0 +1,26 @@
+package com.smalldogg.jpashop.repository;
+
+import com.smalldogg.jpashop.domain.Address;
+import com.smalldogg.jpashop.domain.Order;
+import com.smalldogg.jpashop.domain.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name; //LAZY 초기화
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address; //LAZY 초기화
+    }
+}
