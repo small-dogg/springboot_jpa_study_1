8de1d3b42c446bd73a60df90992160f829e59ddc
small-dogg <sy920112@naver.com>
2022-04-28 10:13:16 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화  - 간단한 주문 조회 V1: 엔티티를 직접 노출  - 간단한 주문 조회 V2: 엔티티를 DTO로 변환  - 간단한 주문 조회 V3: 페치 조인 최적화

--- src/main/java/com/smalldogg/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/smalldogg/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package com.smalldogg.jpashop.api;
 
+import com.smalldogg.jpashop.domain.Address;
 import com.smalldogg.jpashop.domain.Order;
+import com.smalldogg.jpashop.domain.OrderStatus;
 import com.smalldogg.jpashop.repository.OrderRepository;
 import com.smalldogg.jpashop.repository.OrderSearch;
+import com.smalldogg.jpashop.repository.OrderSimpleQueryDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * XtoOne 관계(ManyToOne, OneToOne) 안에서의 성능 최적화
@@ -32,5 +39,53 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    // Order 2개
+    // N + 1 문제가 발생 -> 1+ 회원 N + 배송 N
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    //Order 2개
+    // fetch join으로 한방 쿼리로 처리가 되었다.
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    // v3 와의 trade-off
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
+    }
+
+
+
+    @Data
+    @AllArgsConstructor
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
